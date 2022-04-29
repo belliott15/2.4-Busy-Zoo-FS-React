f5e40777fce6d45708426df188792f9cfdb4d4e3
Beau Elliott <belliott1516@gmail.com>
2022-04-29 14:49:30 -0700

    finished styling on battleArena

--- src/Components/BattleArena/BattleArena.js
+++ src/Components/BattleArena/BattleArena.js
@@ -8,15 +8,19 @@ export default function BattleArena({ chimeraHealth, setChimeraHealth, griffinHe
       {isOpen ?
         <div className='battle-arena' >
           <div className='monster'>
-            <img width={chimeraHealth * 11} src='./chimera.png' />
-            <button onClick={() => setChimeraHealth(chimeraHealth + 1)}>Increase Health</button>
-            <button onClick={() => setGriffinHealth(griffinHealth - 1)}>Attack</button>
+            <img width={chimeraHealth * 12} src='./chimera.png' />
+            <div className='button-container'>
+              <button onClick={() => {chimeraHealth < 22 ? setChimeraHealth(chimeraHealth + 1) : setChimeraHealth(chimeraHealth + 0);}}>Increase Health</button>
+              <button onClick={() => {griffinHealth > 3 ? setGriffinHealth(griffinHealth - 1) : setGriffinHealth(griffinHealth - 0);}}>Attack</button>
+            </div>
           </div>
           <img className='vsImage' src='./vsImage.png' />
           <div className='monster'>
-            <img width={griffinHealth * 11} src='./griffin.png' />
-            <button onClick={() => setGriffinHealth(griffinHealth + 1)}>Increase Health</button>
-            <button onClick={() => setChimeraHealth(chimeraHealth - 1)}>Attack</button>
+            <img width={griffinHealth * 12} src='./griffin.png' />
+            <div className='button-container'>
+              <button onClick={() => {griffinHealth < 21 ? setGriffinHealth(griffinHealth + 1) : setGriffinHealth(griffinHealth + 0); }}>Increase Health</button>
+              <button onClick={() => {chimeraHealth > 3 ? setChimeraHealth(chimeraHealth - 1) : setChimeraHealth(chimeraHealth - 0);}}>Attack</button>
+            </div>
           </div>
         </div>
         : ''}
