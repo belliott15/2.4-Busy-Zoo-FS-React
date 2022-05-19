4e75c61936fe6797385e4d3ceb8ef3329f572d0d
Beau Elliott <belliott1516@gmail.com>
2022-05-18 17:40:36 -0700

    created two tests in app.test.js for the parade buttons and open close buttons

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+// import Parade from './Components/Parade/Parade';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+
+test('on button click renders the creature identified on the button', () => {
+  render(<App />);
+  
+  const openButton = screen.queryByText(/Open up/i);
+  fireEvent.click(openButton);
+
+  const emojiElement = screen.queryAllByText(/🧜‍♀️/i);
+  expect(emojiElement.length).toBe(1);
+
+  const mermaidButton = screen.queryByText(/Mermaid/i);
+  expect(mermaidButton).toBeInTheDocument();
+  fireEvent.click(mermaidButton);
+
+  const emojiElementAfter = screen.queryAllByText(/🧜‍♀️/i);
+  expect(emojiElementAfter.length).toBe(2);
+
+  const dragonButton = screen.queryByText(/Dragon/i);
+  fireEvent.click(dragonButton);
+
+  const emojiDragon = screen.queryByText(/🐉/i);
+  expect(emojiDragon).toBeInTheDocument();
+});
+
+test('on button click opens the zoo and then closes it', () => {
+  render(<App />);
+  const openButton = screen.queryByText(/Open up/i);
+  const closedText = screen.queryByText(/Sorry We Are Closed/i);
+  expect(closedText).toBeInTheDocument();
+  fireEvent.click(openButton);
+
+  const openText = screen.queryByText(/Come in!/i);
+  expect(openText).toBeInTheDocument();
+
+  const closeButton = screen.queryByText(/Close it down/i);
+  fireEvent.click(closeButton);
+
+  expect(closedText).toBeInTheDocument();
+  
 });
