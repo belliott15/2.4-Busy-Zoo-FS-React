d996ce2c73cb25e22387f5d5d3b8e2e1dccc4be4
Beau Elliott <belliott1516@gmail.com>
2022-05-20 10:32:46 -0700

    trying to pass ci

--- src/Components/AdminPage/MyTable.js
+++ src/Components/AdminPage/MyTable.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import MaterialTable from 'material-table';
 import { customerData } from '../../customerData';
 import './AdminPage.css';
